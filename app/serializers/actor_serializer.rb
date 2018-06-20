2c5bf8c89b28f9933c5dc04b080ccef5ab825ea3
briecodes <hello@brie.work>
2018-06-20 17:46:40 -0400

    id added to actor serializer.

--- app/serializers/actor_serializer.rb
+++ app/serializers/actor_serializer.rb
@@ -1,5 +1,5 @@
 class ActorSerializer < ActiveModel::Serializer
-  attributes :name, :cv, :img_url
+  attributes :name, :cv, :img_url, :id
 
   has_many :casts
   has_many :productions, through: :casts
