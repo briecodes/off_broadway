82f7075b2f322f6ca2f0669df1a3865e9367ed28
briecodes <hello@brie.work>
2018-07-23 12:00:09 -0400

    session create error testing.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(username: params['username'])
     if (@user && @user.authenticate(params['password']))
-      token = create_token({username: @user.username, user_id: @user.id})
+      # token = create_token({username: @user.username, user_id: @user.id})
       render json: {
         user: @user
         # user: @user,
