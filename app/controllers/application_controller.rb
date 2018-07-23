e9b522a4ebc1451efd9011e21dbe5c8b50f67757
briecodes <hello@brie.work>
2018-07-23 11:10:35 -0400

    application controller defined with JWT.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,46 @@
 class ApplicationController < ActionController::API
+  def get_secret
+    ENV['JWT_SECRET']
+  end
+
+  def create_token(payload)
+    token = JWT.encode payload, ENV['JWT_SECRET'], 'HS256'
+  end
+
+  def get_token
+    request.headers['Authorization']
+  end
+
+  def get_decoded_token
+    token = get_token()
+    begin
+      decoded_token = JWT.decode token, get_secret(), true, { algorithm: 'HS256' }
+    rescue JWT::DecodeError
+      return nil
+    end
+    decoded_token
+  end
+
+  def requires_login
+    if !is_authenticated?
+      render json: {
+        message: 'Warning!',
+        error: 'Not authorized.'
+      }, status: :unauthorized
+    end
+  end
+
+  def is_authenticated?
+    !!get_decoded_token
+  end
+
+  def requires_user_match
+    @user = User.find_by(id: params[:user_id])
+    if @user.id != get_decoded_token[0]['id']
+      render json: {
+        message: 'Unauthorized User.'
+      }, status: :unauthorized
+    end
+  end
+
 end
