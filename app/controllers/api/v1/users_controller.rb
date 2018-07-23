935ea5cddda8f6525fd8a02ba8bb64dbc66fd5dc
briecodes <hello@brie.work>
2018-07-23 11:14:14 -0400

    create method refactored to use JWT.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UsersController < ApplicationController
     before_action :get_user, only: [:show, :update]
+    before_action :requires_login, only: [:index, :show, :update, :destroy]
 
     def index
         users = User.all
@@ -13,17 +14,20 @@ class Api::V1::UsersController < ApplicationController
         render json: @user
     end
 
-    # def new
-    #     @user = User.new
-    # end
-
     def create
-        user = User.create(user_params)
-        if user.save
-            render json: @user
-        else
-            render json: {error: 'Something went wrong!'}
-        end
+      user = User.create(user_params)
+      if user.save
+        payload = {
+          username: user.username
+        }
+        token = JWT.encode payload, ENV['JWT_SECRET'], 'HS256'
+        render json: {
+          token: token,
+          user: user
+        }
+      else
+        render json: {errors: user.errors.full_messages}, status: :unprocessible_entity
+      end
     end
 
     def update
