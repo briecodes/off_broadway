7ca4f175c38f33bd867da4e885ec8c8e67764bd1
briecodes <hello@brie.work>
2018-07-23 11:19:47 -0400

    sessions controller created and defined.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  def create
+    @user = User.find_by(username: params['username'])
+    if (@user && @user.authenticate(params['password']))
+      token = create_token({username: @user.username, user_id: @user.id})
+      render json: {
+        user: @user,
+        token: token
+      }
+    else
+      render json: {
+        errors: 'Those credentials do not match anything we have in our database.'
+      }, status: :unauthorized
+    end
+  end
+end
