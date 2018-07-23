3e937410e8728ad516b8c604c51385ba8970a684
briecodes <hello@brie.work>
2018-07-23 10:15:05 -0400

    user class fleshed out with validation and has password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
-    has_many :scripts
-    has_many :productions
+  has_secure_password
+
+  has_many :scripts
+  has_many :productions
+
+  validates :username, uniqueness: true
+  validates :username, format: { without: /\s/ }
+  validates :username, presence: true
+  validates :password, presence: true
 end
